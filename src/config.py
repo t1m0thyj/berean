b9c3a9cdd02a393a282e6d0cb3c9057e6b05ed69
Timothy Johnson <tandrewjohnson@verizon.net>
2015-05-26 21:05:29 -0400

    Fixed bug where auto-complete setting was not applied correctly. Updated version number and copyright dates.

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 """config.py - global variables used throughout Berean"""
 
-VERSION = "0.4.9.2"
+VERSION = "0.4.10"
 
 VERSION_NAMES = (
     "ASV", "BBE", "Darby", "DutSVV", "FreSegond", "GerLut1545", "JPS", "KJV",
